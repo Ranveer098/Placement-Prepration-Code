a13fe0adec3aa1c3122978168bcb32dde7b466b8
Ranveer098 <95100082+Ranveer098@users.noreply.github.com>
2024-03-28 19:54:01 +0530

    done

--- Linked_list/palidrome.cpp
+++ Linked_list/palidrome.cpp
@@ -12,6 +12,39 @@ struct Node
   }
 };
 
+Node *reverse_(Node *head)
+{
+  if (head == NULL || head->next == NULL)
+    return head;
+  Node *rest_head = reverse_(head->next);
+  Node *rest_tail = head->next;
+  rest_tail->next = head;
+  head->next = NULL;
+  return rest_head;
+}
+
+ bool ispalidrome__(Node *head){
+  if(head==NULL) return true;
+  Node *fast=head; Node*slow=head;
+  while (fast->next!=NULL && fast->next->next!=NULL)
+  {
+    slow=slow->next;
+    fast=fast->next->next;
+  }
+  Node *rev=reverse_(slow->next);
+  Node *curr=head;
+  while (rev!=NULL)
+  {
+    if(rev->data!=curr->data) return false;
+    rev=rev->next;
+    curr=curr->next;
+    
+  }
+  return true;
+  
+  
+ }
+// Naive solution
 bool isPalindrome(Node *head)
 {
   stack<char> st;
@@ -30,12 +63,14 @@ bool isPalindrome(Node *head)
   return true;
 }
 
+// efficient solution
+
 int main()
 {
   Node *head = new Node('g');
   head->next = new Node('f');
-  head->next->next = new Node('l');
-  if (isPalindrome(head))
+  head->next->next = new Node('g');
+  if (ispalidrome__(head))
     cout << "Yes";
   else
     cout << "No";
